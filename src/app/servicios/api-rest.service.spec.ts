8b60063706cd81e4d249bfc20db7bb8fd6eb37e4
AsdUserName <juanmonsalveztn@gmail.com>
2023-11-23 02:09:50 -0300

    cambiando de api rest

--- src/app/servicios/api-rest.service.spec.ts
+++ src/app/servicios/api-rest.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from './api.service';
+import { ApiRestService } from './api-rest.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
+describe('ApiRestService', () => {
+  let service: ApiRestService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    service = TestBed.inject(ApiRestService);
   });
 
   it('should be created', () => {
